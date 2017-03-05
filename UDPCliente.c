bcf40aa5459d40395998c8d5f29046f01a26dfb8
leobmaffei <leobmaffei@gmail.com>
2017-03-05 15:18:12 -0300

    resposta WP

--- UDPCliente.c
+++ UDPCliente.c
@@ -15,11 +15,11 @@ char **argv;
 {
 
 
-   int s;
-   unsigned short port;
-   struct sockaddr_in server;
+   int sockint,s, sResposta, namelen, client_address_size; //neste caso o endereco do cliente é o servidor
+   unsigned short port, portResposta;
+   struct sockaddr_in client, server;
    char buf[200];
-   char input[200] = "exit\n";  //Adicionado o \n para comparacao
+   char input[200] = "exit\n";  //Adicionado o \n para comparacao Show mano
    /* 
     * O primeiro argumento (argv[1]) é o endereço IP do servidor.
     * O segundo argumento (argv[2]) é a porta do servidor.
@@ -30,7 +30,8 @@ char **argv;
       exit(1);
    }
   port = htons(atoi(argv[2]));
-  while(strcmp(buf,input) != 0) {
+  portResposta = htons(atoi("5454"));
+  
    /*
     * Cria um socket UDP (dgram).
     */
@@ -39,28 +40,28 @@ char **argv;
        perror("socket()");
        exit(1);
    }
+   if ((sResposta = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+   {
+       perror("socket()");
+       exit(1);
+   }
+  
 
    /* Define o endereço IP e a porta do servidor */
    server.sin_family      = AF_INET;            /* Tipo do endereço         */
    server.sin_port        = port;               /* Porta do servidor        */
    server.sin_addr.s_addr = inet_addr(argv[1]); /* Endereço IP do servidor  */
+    
 
+
+    while(strcmp(buf,input) != 0) {
    //strcpy(buf, "Hello");
 
    /*Pega a mensagem do teclado para ser enviada*/
 
       printf(">");
       fgets(buf, sizeof buf, stdin);
-      //teste para ver o erro -------------------------------------------------
-      
-      
-    if(strcmp(buf,input) == 0){
-      printf("igual\n");
-    }
-    if(strcmp(buf,input) != 0){
-      printf("diferente\n");
-    }
-//------------------------------------------------------------------------------
+
 
     /* Envia a mensagem no buffer para o servidor */
       if (sendto(s, buf, (strlen(buf)+1), 0, (struct sockaddr *)&server, sizeof(server)) < 0)
@@ -68,10 +69,59 @@ char **argv;
           perror("sendto()");
           exit(2);
       }
-          close(s);
-   /* Fecha o socket */
-      }
-
+          
       
+
+
+          //daqui para baixo ele vira o servidor esperando a resposta
+      /*
+    * Liga o servidor à porta definida anteriormente.
+    */
+         /* Define o endereço IP e a porta do cliente paea resposta */
+   server.sin_family      = AF_INET;            /* Tipo do endereço         */
+   server.sin_port        = portResposta;               /* Porta do servidor        */
+   server.sin_addr.s_addr = INADDR_ANY; /* Endereço IP do servidor  */
+
+
+     if (bind(sResposta, (struct sockaddr *)&server, sizeof(server)) < 0) //////////////////////////////////////socket sResposta para receber a resposta do server
+     {
+       perror("bind()");
+       exit(1);
+     }
+
+      /* Consulta qual porta foi utilizada. */
+     namelen = sizeof(server);
+     if (getsockname(s, (struct sockaddr *) &server, &namelen) < 0)
+     {
+       perror("getsockname()");
+       exit(1);
+     }
+
+   /* Imprime qual porta foi utilizada. */
+     printf("Porta utilizada é %d\n", ntohs(server.sin_port));
+
+
+     /*
+    * Recebe uma mensagem do cliente.
+    * O endereço do cliente será armazenado em "client".
+    */
+     client_address_size = sizeof(client);
+    if(recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *) &client,
+      &client_address_size) <0)
+     {
+       perror("recvfrom()");
+       exit(1);
+     }
+      /*
+      * Imprime a mensagem recebida, o endereço IP do cliente
+      * e a porta do cliente 
+      */
+     printf("Recebida a mensagem: %s \n IP: %s PORTA: %d\n",buf,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
+       
+
+      }
+      /* Fecha o socket */
+      
+      close(sResposta);
     exit(1);
 }
--- UDPServer.c
+++ UDPServer.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 //#include <sys/cstdlib.h> // Para o clear
 
 
@@ -12,29 +13,23 @@
  */
 main(int argc, char *argv[] )
 {
-    
-    unsigned short port;
-   int sockint,s, namelen, client_address_size;
-   struct sockaddr_in client, server;
-   char buf[200];
-   char exitFlag = "n";
 
-   /*
-    * Cria um socket UDP (dgram). 
-    */
-   if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-   {
-       perror("socket()");
-       exit(1);
-   }
+  unsigned short port;
+  int sockint,s, namelen, client_address_size;
+  struct sockaddr_in client, server;
+  char buf[200];
+  char bufResposta[200];
+  char exitFlag = "n";
+  int flagComandoSistema; 
 
-   /*
+
+/*
     * Define a qual endereço IP e porta o servidor estará ligado.
     * Porta = 0 -> faz com que seja utilizada uma porta qualquer livre.
     * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
     * os endereços IP
     */
-    
+
     port = (unsigned short) atoi(argv[1]);// ******* Cast para short da Porta Digitada
     
     
@@ -42,54 +37,101 @@ main(int argc, char *argv[] )
     server.sin_port        = htons(port);         /* Escolhe uma porta disponível */
    server.sin_addr.s_addr = INADDR_ANY;/* Endereço IP do servidor      */
 
-    
+    while(buf != "exit")
+    {
+   /*
+    * Cria um socket UDP (dgram). 
+    */
+     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+     {
+       perror("socket()");
+       exit(1);
+     }
+
    /*
     * Liga o servidor à porta definida anteriormente.
     */
-   if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-   {
+     if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+     {
        perror("bind()");
        exit(1);
-   }
+     }
 
    /* Consulta qual porta foi utilizada. */
-   namelen = sizeof(server);
-   if (getsockname(s, (struct sockaddr *) &server, &namelen) < 0)
-   {
+     namelen = sizeof(server);
+     if (getsockname(s, (struct sockaddr *) &server, &namelen) < 0)
+     {
        perror("getsockname()");
        exit(1);
-   }
+     }
 
    /* Imprime qual porta foi utilizada. */
-   printf("Porta utilizada é %d\n", ntohs(server.sin_port));
+     printf("Porta utilizada é %d\n", ntohs(server.sin_port));
 
    /*
     * Recebe uma mensagem do cliente.
     * O endereço do cliente será armazenado em "client".
     */
-   client_address_size = sizeof(client);
+     client_address_size = sizeof(client);
 
-   while(buf != "exit")
-   {
+
+      //system("clear");
 
      if(recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *) &client,
-              &client_address_size) <0)
+      &client_address_size) <0)
      {
-         perror("recvfrom()");
-         exit(1);
+       perror("recvfrom()");
+       exit(1);
      }
-      system("CLS");
-      printf("SERVIDOR:\n");
-     /*
+      /*
       * Imprime a mensagem recebida, o endereço IP do cliente
       * e a porta do cliente 
       */
      printf("Recebida a mensagem: %s \n IP: %s PORTA: %d\n",buf,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
-   /*
+
+    /*
     * Fecha o socket.
     */
-   }
-   close(s);
- 
- exit(1);
+
+     close(s);
+     printf("fexando socket de recebimento\n");
+     // removido o atoi pois acredito nao precisar pois ja está em int
+     port = client.sin_port;
+      printf("porta de envio %s\n", ntohs(port));
+
+
+     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+     {
+       perror("socket()");
+       exit(1);
+     }
+      printf("sem erro de socket\n");
+
+   /* Define o endereço IP e a porta do servidor */
+   server.sin_family      = AF_INET;            /* Tipo do endereço         */
+   server.sin_port        = port;               /* Porta do servidor        */
+   server.sin_addr.s_addr = inet_addr(inet_ntoa(client.sin_addr)); /* Endereço IP do servidor  */
+         printf("criando tipo, porta e IP\n");
+
+   //garda o retorno da função no buffer de resposta
+     fgets(bufResposta, sizeof(bufResposta), "Teste de resposta ao cliente");
+          printf("salva string no bufResposta\n");
+
+/* Envia a mensagem no buffer para o servidor */
+     if (sendto(s, bufResposta, (strlen(bufResposta)+1), 0, (struct sockaddr *)&server, sizeof(server)) < 0)
+     {
+      perror("sendto()");
+      exit(2);
+    }
+         printf("mensagem retornada para o cliente com sucesso\n");
+
+    close(s);
+         printf("porta de resposta fechada\n");
+
+   /* Fecha o socket */ 
+
+  }
+
+
+  exit(1);
 }
